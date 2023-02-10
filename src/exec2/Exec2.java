1b5c3d88baaf50e1f1b96cbf0cffe3a9a7170c89
Italo Chagas <47859575+ItaloChagas@users.noreply.github.com>
2023-02-09 21:26:50 -0300

    Update Exec2.java

--- src/exec2/Exec2.java
+++ src/exec2/Exec2.java
@@ -2,38 +2,49 @@ package exec2;
 
 public class Exec2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] vetor = {11,3,43,55,67,55,32,357,88,343};
-		
-		ordena(vetor);
-	}
-	
-	public static void ordena(int[] vetor) {
-		int maior_valor=0;
-		int maior_indice=0;
-		
-		for(int i =0; i < vetor.length-1; i++) {
-			maior_valor= vetor[i];
-			maior_indice=i;
-			
-			for(int j =i+1; j< vetor.length; j++) {
-				
-				if(maior_valor < vetor[j]) {
-					maior_valor = vetor[j];
-					maior_indice = j;
-				}
-			}
-			
-			vetor[maior_indice] = vetor[i];
-			vetor[i] = maior_valor;
-			
-		}
-		
-		for(int i =0; i<vetor.length; i++) {
-			
-			System.out.println(vetor[i]);
-		}
-	}
+        int[] vetor = {
+            11,
+            3,
+            43,
+            55,
+            67,
+            55,æ
+            32,
+            357,
+            88,
+            343
+        };
+
+        ordena(vetor);
+    }
+
+    public static void ordena(int[] vetor) {
+        int maior_valor = 0;
+        int maior_indice = 0;
+
+        for (int i = 0; i < vetor.length - 1; i++) {
+            maior_valor = vetor[i];
+            maior_indice = i;
+
+            for (int j = i + 1; j < vetor.length; j++) {
+
+                if (maior_valor < vetor[j]) {
+                    maior_valor = vetor[j];
+                    maior_indice = j;
+                }
+            }
+
+            vetor[maior_indice] = vetor[i];
+            vetor[i] = maior_valor;
+
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+
+            System.out.println(vetor[i]);
+        }
+    }
 
 }
